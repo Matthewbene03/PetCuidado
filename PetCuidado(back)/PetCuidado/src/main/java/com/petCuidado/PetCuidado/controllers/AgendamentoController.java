2135ec08f406bed958649d7c3cc10fa7368873d1
Arbusto1 <guto58ra@gmail.com>
2025-07-20 14:19:48 -0300

    Implemetação da documentação por meio do swagger para as entidades pagamentos, agendamentos, e serviços

--- PetCuidado(back)/PetCuidado/src/main/java/com/petCuidado/PetCuidado/controllers/AgendamentoController.java
+++ PetCuidado(back)/PetCuidado/src/main/java/com/petCuidado/PetCuidado/controllers/AgendamentoController.java
@@ -16,57 +16,84 @@ import org.springframework.web.bind.annotation.RestController;
 import com.petCuidado.PetCuidado.entitiesDTO.AgendamentoDTO;
 import com.petCuidado.PetCuidado.services.AgendamentoService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping("/agendamentos")
+@Tag(name = "Agendamento", description = "Operações relacionadas a Agendamentos")
 public class AgendamentoController {
 	
 	@Autowired
 	private AgendamentoService agendamentoService;
 	
 	@GetMapping("/{id}") 
-	public ResponseEntity<AgendamentoDTO> findById(@PathVariable Long id) { 
+	@Operation(summary = "Busca agendamento por ID", description = "Retorna os dados de um agendamento em específico")
+	public ResponseEntity<AgendamentoDTO> findById(
+			@Parameter(description = "ID do agendamento a ser buscado", example = "1")
+			@PathVariable Long id) { 
 		AgendamentoDTO agendamentoDTO = agendamentoService.findById(id); 
 		return ResponseEntity.ok(agendamentoDTO); 
 	}
 	
-	@GetMapping 
+	@GetMapping
+	@Operation(summary = "Busca todos os agendamentos", description = "Retorna a lista de todos os agendamentos cadastrados")
 	public ResponseEntity<List<AgendamentoDTO>> findAll() { 
 		List<AgendamentoDTO> AgendamentoDTOs = agendamentoService.findAll(); 
 		return ResponseEntity.ok(AgendamentoDTOs); 
 	}
 	
 	@PostMapping 
-	public ResponseEntity<AgendamentoDTO> create(@RequestBody AgendamentoDTO agendamentoDTO) { 
+	@Operation(summary = "Cria agendamento", description = "Cria um novo cadastro de agendamento no banco")
+	public ResponseEntity<AgendamentoDTO> create(
+			@Parameter(description = "Objeto DTO de um agendamento")
+			@RequestBody AgendamentoDTO agendamentoDTO) { 
 		AgendamentoDTO novoAgendamento = agendamentoService.insert(agendamentoDTO); 
 		return ResponseEntity.status(201).body(novoAgendamento); 
 	}
 	
 	@PutMapping("/{id}") 
-	public ResponseEntity<AgendamentoDTO> update(@PathVariable Long id, @RequestBody AgendamentoDTO agendamentoDTO) { 
+	@Operation(summary = "Atualiza um agendamento", description = "Atualiza um agendamento já existente")
+	public ResponseEntity<AgendamentoDTO> update(
+			@Parameter(description = "ID do agendamento que você quer atualizar mais o objeto DTO com os dados do agendamento atualizados")
+			@PathVariable Long id, @RequestBody AgendamentoDTO agendamentoDTO) { 
 		AgendamentoDTO agendamentoAtualizado = agendamentoService.update(id, agendamentoDTO); 
 		return ResponseEntity.ok(agendamentoAtualizado); 
 	}
 	
 	@DeleteMapping("/{id}") 
-	public ResponseEntity<Void> delete(@PathVariable Long id) { 
+	@Operation(summary = "Deleta um agendamento", description = "Deleta um agendamento com base em seu ID")
+	public ResponseEntity<Void> delete(
+			@Parameter(description = "ID do agendamento a ser deletado", example = "1")
+			@PathVariable Long id) { 
 		agendamentoService.delete(id); 
 		return ResponseEntity.noContent().build(); 
 	}
 	
 	@GetMapping("/servico/{servicoId}")
-	public ResponseEntity<List<AgendamentoDTO>> findByServicoId(@PathVariable long servicoId) {
+	@Operation(summary = "Busca agendamentos com base no ID do Serviço", description = "Retorna uma lista de todos os agendamentos de um determindado Serviço")
+	public ResponseEntity<List<AgendamentoDTO>> findByServicoId(
+			@Parameter(description = "ID do serviço", example = "1")
+			@PathVariable long servicoId) {
 		List<AgendamentoDTO> agendamentoDTOs = agendamentoService.findByServicoId(servicoId);
 		return ResponseEntity.ok(agendamentoDTOs);
 	}
 	
 	@GetMapping("/pet/{petId}")
-	public ResponseEntity<List<AgendamentoDTO>> findByPetId(@PathVariable long petId) {
+	@Operation(summary = "Busca agendamentos com base no ID do Pet", description = "Retorna uma lista de todos os agendamentos de um determindado Pet")
+	public ResponseEntity<List<AgendamentoDTO>> findByPetId(
+			@Parameter(description = "ID do pet", example = "1")
+			@PathVariable long petId) {
 		List<AgendamentoDTO> agendamentoDTOs = agendamentoService.findByPetId(petId);
 		return ResponseEntity.ok(agendamentoDTOs);
 	}
 	
 	@GetMapping("/funcionario/{funcionarioId}")
-	public ResponseEntity<List<AgendamentoDTO>> findByFuncionarioId(@PathVariable long funcionarioId) {
+	@Operation(summary = "Busca agendamentos com base no ID do Funcionario", description = "Retorna uma lista de todos os agendamentos de um determindado Funcionario")
+	public ResponseEntity<List<AgendamentoDTO>> findByFuncionarioId(
+			@Parameter(description = "ID do funcionario", example = "1")
+			@PathVariable long funcionarioId) {
 		List<AgendamentoDTO> agendamentoDTOs = agendamentoService.findByFuncionarioId(funcionarioId);
 		return ResponseEntity.ok(agendamentoDTOs);
 	}
--- PetCuidado(back)/PetCuidado/src/main/java/com/petCuidado/PetCuidado/controllers/ServicoController.java
+++ PetCuidado(back)/PetCuidado/src/main/java/com/petCuidado/PetCuidado/controllers/ServicoController.java
@@ -16,38 +16,57 @@ import org.springframework.web.bind.annotation.RestController;
 import com.petCuidado.PetCuidado.entitiesDTO.ServicoDTO;
 import com.petCuidado.PetCuidado.services.ServicoService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping("/servicos")
+@Tag(name = "Serviço", description = "Operações relacionadas a um Serviço")
 public class ServicoController {
 
 	@Autowired
 	private ServicoService servicoService;
 	
 	@GetMapping("/{id}") 
-	public ResponseEntity<ServicoDTO> findById(@PathVariable Long id) { 
+	@Operation(summary = "Busca serviço por ID", description = "Retorna os dados de um serviço em específico")
+	public ResponseEntity<ServicoDTO> findById(
+			@Parameter(description = "ID do serviço a ser buscado", example = "1")
+			@PathVariable Long id) { 
 		ServicoDTO servicoDTO = servicoService.findById(id); 
 		return ResponseEntity.ok(servicoDTO); 
 	}
 	
-	@GetMapping public ResponseEntity<List<ServicoDTO>> findAll() {
+	@GetMapping 
+	@Operation(summary = "Busca todos os serviços", description = "Retorna a lista de todos os serviços cadastrados")
+	public ResponseEntity<List<ServicoDTO>> findAll() {
 		List<ServicoDTO> servicoDTO = servicoService.findAll(); 
 		return ResponseEntity.ok(servicoDTO); 
 	}
 	
 	@PostMapping 
-	public ResponseEntity<ServicoDTO> create(@RequestBody ServicoDTO servicoDTO) { 
+	@Operation(summary = "Cria serviço", description = "Cria um novo cadastro de serviço no banco")
+	public ResponseEntity<ServicoDTO> create(
+			@Parameter(description = "Objeto DTO de um serviço")
+			@RequestBody ServicoDTO servicoDTO) { 
 		ServicoDTO novoServico = servicoService.insert(servicoDTO); 
 		return ResponseEntity.status(201).body(novoServico); 
 	}
 	
-	@PutMapping("/{id}") 
-	public ResponseEntity<ServicoDTO> update(@PathVariable Long id, @RequestBody ServicoDTO servicoDTO) { 
+	@PutMapping("/{id}")
+	@Operation(summary = "Atualiza um serviço", description = "Atualiza um serviço já existente")
+	public ResponseEntity<ServicoDTO> update(
+			@Parameter(description = "ID do serviço que você quer atualizar mais o objeto DTO com os dados do serviço atualizados")
+			@PathVariable Long id, @RequestBody ServicoDTO servicoDTO) { 
 		ServicoDTO servicoAtualizado = servicoService.update(id, servicoDTO); 
 		return ResponseEntity.ok(servicoAtualizado); 
 	}
 	
 	@DeleteMapping("/{id}") 
-	public ResponseEntity<Void> delete(@PathVariable Long id) { 
+	@Operation(summary = "Deleta um serviço", description = "Deleta um serviço com base em seu ID")
+	public ResponseEntity<Void> delete(
+			@Parameter(description = "ID do serviço a ser deletado", example = "1")
+			@PathVariable Long id) { 
 		servicoService.delete(id); 
 		return ResponseEntity.noContent().build(); 
 	}
